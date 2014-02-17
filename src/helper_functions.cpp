f86f965223b5dbae006eccd9b9bdc7fa537026f1
Kristoffer Brækken <k@braekken.org>
2014-02-17 10:37:43 +0100

    Wrote skeleton of functions. Returns 0.

--- src/helper_functions.cpp
+++ src/helper_functions.cpp
@@ -1,1 +1,18 @@
 #include "helper_functions.hpp";
+
+/*
+ * @return Reaction rates.
+ */
+double epsilon(double T, double rho) {
+  //TODO write function.
+  return 0;
+}
+
+/*
+ * @return tau by reading opacity data file specified as constant inside
+ * function.
+ */
+double kappa(double T, double R, double rho) {
+  //TODO write function.
+  return 0;
+}
