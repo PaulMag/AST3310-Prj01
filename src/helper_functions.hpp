b0ef3f0ee7b1de0922ff0edb3ce059d19a403adc
Kristoffer Brækken <k@braekken.org>
2014-02-17 10:18:18 +0100

    Changed from tau to kappa to correspond with problem text.

--- src/helper_functions.hpp
+++ src/helper_functions.hpp
@@ -7,4 +7,4 @@ double epsilon(double T, double rho);
  * @return tau by reading opacity data file specified as constant inside
  * function.
  */
-double tau(double T, double R, double rho);
+double kappa(double T, double R, double rho);
