2a54dc9b2b036dd10b530f8cbe883adbafc86458
Kristoffer Brækken <k@braekken.org>
2014-02-28 15:59:27 +0100

    Kappa function runs correclty but returns wrong number. Correct row but not correct column. Error in traverse row part?

--- python/SolarCoreModel.py
+++ python/SolarCoreModel.py
@@ -9,7 +9,7 @@ differential equations.
 """
 import numpy as np
 
-from numpy import pi
+from numpy import pi, log
 
 """CONSTANTS"""
 
@@ -21,7 +21,7 @@ def rhs_r(r, rho):
     """
     @return dr / dm
     """
-    return 1. / ( 4. * pi r*r * rho )
+    return 1. / ( 4. * pi * r*r * rho )
 
 def rhs_P(r, m):
     """
@@ -57,7 +57,7 @@ def kappa(T, rho, opacityFile=OPACITY_FILE):
     R = float(rho) / ( T / 1e6 )
     log10R = log(R)
 
-    # Traverse file to find correct log10kappa
+    # Begin process of traversing file to find correct log10kappa
     opacities = open(opacityFile, 'r')
 
     """
@@ -69,12 +69,83 @@ def kappa(T, rho, opacityFile=OPACITY_FILE):
     headerline = \
             np.asarray( opacities.readline().strip().split()[1:], dtype=np.float64 )
 
+    opacities.close()
+
     closest = headerline[0]
     for T in headerline[1:]:
-        if abs(log10T-T) > abs(log10T-closes):
+        if abs(log10T-T) > abs(log10T-closest):
             closest = T
 
     # Which column does this T correspond to?
+    columnNo = 0
+    for T in headerline:
+        if T == closest:
+            break
+        columnNo += 1
+
+    # Using the log10R, can now find the correct row to use
+    opacities = open(opacityFile, 'r')
+    opacities.readline() # Skip first line
+
+    closest = False
+    for line in opacities:
+        if len(line.strip()) == 0: continue # Skip empty lines
+
+        row = \
+            np.asarray( line.strip().split(), dtype=np.float64 )
+
+        if closest == False:
+            closest = row[0]
+
+        if abs(log10R-row[0]) < abs(log10R-closest):
+            closest = row[0]
+
+    opacities.close()
+
+    # Which row does this log(R) correspond to?
+    opacities = open(opacityFile, 'r')
+    opacities.readline() # Skip first line
+
+    rowNo = 0
+    for line in opacities:
+        if len(line.strip()) == 0: continue # Skip empty lines
+
+        row = \
+            np.asarray( line.strip().split(), dtype=np.float64 )
+
+        if row[0] == closest: break
+
+        rowNo += 1
+
+    opacities.close()
+
+    # Fetch correct value
+    opacities = open(opacityFile, 'r')
+    opacities.readline() # Skip first line
+
+    current_row = 0
+    current_column = 0
+    for line in opacities:
+        if len(line.strip()) == 0: continue # Skip empty lines
+
+        row = \
+            np.asarray( line.strip().split(), dtype=np.float64 )
+
+        if current_row == rowNo:
+            for k in row[1:]:
+                if current_column == columnNo:
+                    log10kappa = k
+                    break
+                current_column += 1
+        current_row += 1
+
+    opacities.close()
 
     # Returned from file is log10kappa
+    print log10kappa
     return 10**log10kappa
+
+if __name__ == '__main__':
+    T = 10**(3.75)
+    rho = 5.623413252e-8
+    print kappa(T, rho)
